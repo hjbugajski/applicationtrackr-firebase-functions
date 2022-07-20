876d61dc894210bf559d9cdcbf54ff9f34060ba0
Henry Bugajski <hjbugajski@gmail.com>
2022-07-19 21:52:06 -0400

    feat: implement recursiveDelete function

---
+++ src/enums/reference-types.enum.ts
@@ -0,0 +1,4 @@
+export enum ReferenceTypes {
+  Doc = 'doc',
+  Collection = 'collection'
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,45 @@
-import * as functions from 'firebase-functions';
+import { initializeApp } from 'firebase-admin/app';
+import { CollectionReference, DocumentReference, getFirestore } from 'firebase-admin/firestore';
+import { https, runWith, logger } from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+import { ReferenceTypes } from './enums/reference-types.enum';
+import { RecursiveDeleteData } from './interfaces/recursive-delete-data.interface';
+
+initializeApp();
+
+const firestore = getFirestore();
+
+export const recursiveDelete = runWith({ timeoutSeconds: 540 }).https.onCall(
+  async (data: RecursiveDeleteData, context) => {
+    if (!context || !context.auth) {
+      throw new https.HttpsError('unauthenticated', 'Request has invalid credentials.');
+    }
+
+    if (!data || !data.path || !data.refType) {
+      throw new https.HttpsError('invalid-argument', 'Request has invalid data.');
+    }
+
+    const path = `users/${context.auth.uid}/${data.path}`;
+    let ref: DocumentReference | CollectionReference;
+
+    logger.info(`User ${context.auth.uid} has requested to delete path ${path}`);
+
+    if (data.refType === ReferenceTypes.Doc) {
+      ref = firestore.doc(path);
+    } else {
+      // ReferenceTypes.Collection
+      ref = firestore.collection(path);
+    }
+
+    return await firestore
+      .recursiveDelete(ref)
+      .then(() => {
+        logger.info('Path has been successfully deleted');
+      })
+      .catch((error) => {
+        logger.error(error);
+
+        throw error;
+      });
+  }
+);
